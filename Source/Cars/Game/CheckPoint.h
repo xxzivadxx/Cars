206268304fc9f8d9b9b53a73d70acacd0882de31
xxzivadxx <davidllanso@hotmail.com>
2018-04-15 14:16:56 +0200

    Quitada funcionalidad de CheckPoints y de FinishLine

--- Source/Cars/Game/CheckPoint.h
+++ Source/Cars/Game/CheckPoint.h
@@ -4,34 +4,16 @@
 #include "Engine/TriggerBox.h"
 #include "CheckPoint.generated.h"
 
-class AFinishLine;
-
 UCLASS()
 class CARS_API ACheckPoint : public ATriggerBox
 {
   GENERATED_BODY()
 
 protected:
-
   // Called when the game starts or when spawned
   virtual void BeginPlay() override;
 
 public:
-
   // constructor sets default values for this actor's properties
   ACheckPoint();
-
-
-  // overlap begin function
-  UFUNCTION()
-    void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
-
-  // overlap end function
-  UFUNCTION()
-    void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
-
-  inline void SetFinishLine(AFinishLine* _pFinishLine) { m_pFinishLine = _pFinishLine; }
-
-private:
-  AFinishLine * m_pFinishLine;
 };
--- Source/Cars/Game/FinishLine.cpp
+++ Source/Cars/Game/FinishLine.cpp
@@ -7,67 +7,10 @@
 
 AFinishLine::AFinishLine()
 {
-  //Register Events
-  OnActorEndOverlap.AddDynamic(this, &AFinishLine::OnOverlapBegin);
 }
 
 // Called when the game starts or when spawned
 void AFinishLine::BeginPlay()
 {
   Super::BeginPlay();
-
-  for (ACheckPoint* pCP : m_vCheckPoints)
-  {
-    pCP->SetFinishLine(this);
-  }
-
-  DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Green, true, -1, 0, 5);
-
-}
-
-void AFinishLine::AddPassedCheckPoint(ACheckPoint* _pCheckPoint)
-{
-  if (!m_vPassedCheckPoints.Contains(_pCheckPoint))
-  {
-    m_vPassedCheckPoints.Add(_pCheckPoint);
-  }
-}
-
-void AFinishLine::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
-{
-  // check if Actors do not equal nullptr and that 
-  if (OtherActor && (OtherActor != this))
-  {
-    float fCurrentTime = GetWorld()->GetTimeSeconds();// UGameplayStatics::GetRealTimeSeconds(GetWorld());
-    if (m_vPassedCheckPoints.Num() == m_vCheckPoints.Num())
-    {
-      m_fLastLapTime = fCurrentTime - m_fPreviousTime;
-      m_fPreviousTime = fCurrentTime;
-      if (m_fBestTime > m_fLastLapTime)
-      {
-        m_fBestTime = m_fLastLapTime;
-        // Put up a debug message for five seconds. The -1 "Key" value (first argument) indicates that we will never need to update or refresh this message.
-        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, *FString("Best Lap!"));
-      }
-      ++m_uLaps;
-
-      GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, *FString("Lap " + FString::FromInt(m_uLaps) + "(" + FString::SanitizeFloat(m_fLastLapTime) + " seconds)"));
-    }
-    else
-    {
-      if (GEngine)
-      {
-        // Put up a debug message for five seconds. The -1 "Key" value (first argument) indicates that we will never need to update or refresh this message.
-        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, *FString("Invalid lap!"));
-      }
-    }
-    m_vPassedCheckPoints.Reset();
-  }
-}
-
-void AFinishLine::OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor)
-{
-  if (OtherActor && (OtherActor != this))
-  {
-  }
 }
