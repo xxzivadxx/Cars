b8af2fee6abcc21403f1bcfe825dc4f99b28e6cd
xxzivadxx <davidllanso@hotmail.com>
2018-04-15 18:09:06 +0200

    Quitada la funcionalidad del control del coche

--- Source/Cars/Game/Car.cpp
+++ Source/Cars/Game/Car.cpp
@@ -1,13 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Car.h"
-#include "GameCamera.h"
-#include "Components/InputComponent.h"
-#include "CarMovementComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/BoxComponent.h"
 #include "ConstructorHelpers.h"
-#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values
@@ -15,7 +11,6 @@ ACar::ACar()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-  m_pCarMovement = CreateDefaultSubobject<UCarMovementComponent>(TEXT("CarMovement"));
   UBoxComponent* BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("RootComponent"));
   RootComponent = BoxComponent;
   m_pMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
@@ -37,38 +32,11 @@ ACar::ACar()
 void ACar::BeginPlay()
 {
 	Super::BeginPlay();
-  m_vMovementInput.Set(0.f, 0.f);
 }
 
 // Called every frame
 void ACar::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-  m_pCarMovement->SetInput(m_vMovementInput);
-}
-
-float ACar::GetVelocityMagnitude()
-{
-  return m_pCarMovement->GetVelocityMagnitude();
-}
-
-// Called to bind functionality to input
-void ACar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-  PlayerInputComponent->BindAxis("Move", this, &ACar::Move);
-  PlayerInputComponent->BindAxis("Turn", this, &ACar::Turn);
-
-}
-
-//Input functions
-void ACar::Move(float AxisValue)
-{
-  m_vMovementInput.Y = FMath::Clamp<float>(AxisValue, -1.0f, 1.0f);
-}
-
-void ACar::Turn(float AxisValue)
-{
-  m_vMovementInput.X = FMath::Clamp<float>(AxisValue, -1.0f, 1.0f);
 }
 
--- Source/Cars/Game/Car.h
+++ Source/Cars/Game/Car.h
@@ -18,25 +18,12 @@ public:
 	ACar();
   // Called every frame
   virtual void Tick(float DeltaTime) override;
-  // Called to bind functionality to input
-  virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-  // Velocity magnitude
-  float GetVelocityMagnitude();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-  //Input functions
-  void Move(float AxisValue);
-  void Turn(float AxisValue);
-
 protected:
-  //Input variables
-  FVector2D m_vMovementInput = FVector2D::ZeroVector;
-  //Movement
-  UPROPERTY(EditAnywhere)
-  UCarMovementComponent* m_pCarMovement;
   //Mesh
   UPROPERTY(EditAnywhere)
   UStaticMeshComponent* m_pMesh;
